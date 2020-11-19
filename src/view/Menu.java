a95f5d57c100e9c9992685925ed50f58a0530157
Arnor <arnorneto10@gmail.com>
2020-11-19 15:49:51 -0300

    New menu options: List Users, delete user. UserPersistence usage. InfraException catch.

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -1,24 +1,41 @@
 package view;
 
 import business.control.UserController;
-import business.exceptions.UserLoginValidationException;
-import business.exceptions.UserPasswordValidationException;
+import util.InfraException;
+import util.UserLoginValidationException;
+import util.UserPasswordValidationException;
 import business.model.User;
+import infra.UserPersistence;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Menu {
     private String menuChooseOptionText() {
         String s = "\n# Escolha a opção:\n";
         s += "\t1 - Criar usuário\n";
+        s += "\t2 - Listar usuários\n";
+        s += "\t3 - Excluir usuário\n";
         s += "\t0 - Sair\n";
         return s;
     }
 
     public void run() {
 
-        while (true) {
-            try {
+        UserController controller;
+
+        File f = new File("users.ser");
+
+        try {
+            if (f.exists()) {
+                controller = UserPersistence.load();
+            } else {
+                controller = new UserController();
+            }
+
+            while (true) {
                 Scanner reader = new Scanner(System.in);
                 System.out.println(this.menuChooseOptionText());
 
@@ -27,28 +44,39 @@ public class Menu {
                 switch (op) {
                     case 0:
                         reader.close();
+                        UserPersistence.save(controller);
                         System.out.println("Saindo da aplicação...");
                         return;
                     case 1:
                         System.out.println("Criar novo usuário");
                         System.out.println("Insira o login:");
                         String login = reader.next();
-                        System.out.println("Insira senha: ");
+                        System.out.println("Insira senha:");
                         String password = reader.next();
                         User toCreateUser = new User(login, password);
-                        new UserController().add(toCreateUser);
+                        controller.add(toCreateUser);
+                        UserPersistence.save(controller);
+                        break;
+                    case 2:
+                        System.out.println("Usuários:");
+                        controller.listAll();
+                        break;
+                    case 3:
+                        System.out.println("Informe o login do usuário:");
+                        String delete = reader.next();
+                        controller.delete(delete);
+                        UserPersistence.save(controller);
                         break;
                     default:
-                        System.out.println("Operação não suportada");
+                        System.out.println("Operação não suportada.");
                         break;
                 }
-
-
-            } catch (UserLoginValidationException | UserPasswordValidationException e) {
-                System.out.println(e.getMessage());
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
             }
+
+        } catch (UserLoginValidationException | UserPasswordValidationException | InfraException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
