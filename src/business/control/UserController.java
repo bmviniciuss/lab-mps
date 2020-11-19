7e94df9b556075f16eca95a21f1b18cb730a7a1e
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2020-11-19 02:58:38 -0300

    feat: add user password validation

--- src/business/control/UserController.java
+++ src/business/control/UserController.java
@@ -1,11 +1,15 @@
 package business.control;
 
 import business.exceptions.UserLoginValidationException;
+import business.exceptions.UserPasswordValidationException;
 import business.model.IUser;
 
 public class UserController {
 
     private int USER_LOGIN_MAX_LENGTH = 20;
+    private int USER_PASSWORD_MIN_LENGTH = 8;
+    private int USER_PASSWORD_MAX_LENGTH = 12;
+
 
     private void validateUserLogin(IUser user) throws UserLoginValidationException {
         if(user.getLogin().isEmpty()) {
@@ -18,6 +22,26 @@ public class UserController {
 
         if(user.getLogin().matches(".*\\d.*")) {
             throw new UserLoginValidationException("Login cannot contain a number");
+        }
+    }
+
+    private void validateUserPassword(IUser user) throws UserPasswordValidationException {
+        if(user.getPassword().isEmpty()) {
+            throw new UserPasswordValidationException("A password must be provided");
+        }
+
+        if(user.getPassword().length() > this.USER_PASSWORD_MAX_LENGTH) {
+            throw new UserPasswordValidationException("Password cannot be longer than "
+                    + this.USER_PASSWORD_MAX_LENGTH);
+        }
+
+        if(user.getPassword().length() < this.USER_PASSWORD_MIN_LENGTH) {
+            throw new UserPasswordValidationException("Password cannot be smaller than "
+                    + this.USER_PASSWORD_MIN_LENGTH);
+        }
+
+        if(!(user.getPassword().matches(".*\\d.*\\d.*") && user.getPassword().matches(".*\\w+.*"))) {
+            throw new UserPasswordValidationException("Password must have letters and at least 2 numbers");
         }
     }
 }
