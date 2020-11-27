04f722efc4359dbbd149da71027e656fa838fbef
Dandara Estrela <dandara_estrela@hotmail.com>
2020-11-26 22:45:25 -0300

    refactor: remove AdminUserController

--- src/business/control/AdminUserControllerFactory.java
+++
@@ -1,7 +0,0 @@
-package business.control;
-
-public class AdminUserControllerFactory {
-    public static AdminUserController getController() {
-        return AdminUserController.getInstance();
-    }
-}
